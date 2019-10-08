89faee11f0b76826937f1c36e3d6bd7e2bce45eb
Kevin Melendez <kmelendez@fdic.gov>
2019-10-08 00:13:33 -0400

    WIP shuffle button

--- src/components/Deck.jsx
+++ src/components/Deck.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import Cards from "./Cards";
-
+import Grid from "@material-ui/core/Grid";
+import Button from "@material-ui/core/Button";
 class Deck extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      cardDeck: [],
+      pickedCards: []
+    };
   }
 
   render() {
+    let id = 1;
+    const initialDeck = this.state.cardDeck;
     const suits = ["♠︎", "♥︎", "♣︎", "♦︎"];
-    const values = [
+    const ranks = [
       "A",
       "1",
       "2",
@@ -25,22 +31,44 @@ class Deck extends React.Component {
       "Q",
       "K"
     ];
-    let cardDeck = [];
-    let card = [];
-
     for (let x = 0; x < suits.length; x++) {
-      for (let y = 0; y < values.length; y++) {
-        card = { suit: suits[x], val: values[y] };
-        cardDeck.push(card);
+      for (let y = 0; y < ranks.length; y++) {
+        let card = { id: id, suit: suits[x], val: ranks[y] };
+        initialDeck.push(card);
+        id++;
       }
     }
 
+    const shuffleDeck = () => {
+      const shuffledDeck = initialDeck;
+      for (let i = shuffledDeck.length - 1; i > 0; i--) {
+        let rand = Math.floor(Math.random() * i);
+        let temp = shuffledDeck[i];
+        shuffledDeck[i] = shuffledDeck[rand];
+        shuffledDeck[rand] = temp;
+      }
+      this.setState({ cardDeck: shuffledDeck, pickedCards: [] });
+    };
+    
     return (
-      <div>
-        {cardDeck.map(function(card) {
-          return <Cards suit={card.suit} value={card.val} />;
-        })}
-      </div>
+      <Grid container spacing={3}>
+        <Grid item={4}>
+          {initialDeck.map(function(card) {
+            return <Cards key={card.id} suit={card.suit} value={card.val} />;
+          })}
+        </Grid>
+        <Grid item={6}>
+          <Button
+            style={{ marginRight: "2rem" }}
+            variant="contained"
+            onClick={() => shuffleDeck()}
+          >
+            Shuffle
+          </Button>
+          <Button variant="contained">Draw Card</Button>
+        </Grid>
+        <Grid item={6}>Cards Drawn Over Here</Grid>
+      </Grid>
     );
   }
 }
